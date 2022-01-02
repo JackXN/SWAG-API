dfefda0027f8aebe3d5293edab5af47ad2f69460
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-01-02 11:23:04 -0700

    login

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
-router.post("/api/auth/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     !user && res.status(401).json("Wrong credentials!");
